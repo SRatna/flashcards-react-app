a7363cba0bf70189447b0c7c3d27478bed5d5b39
sushanta <adhikariratna52@gmail.com>
2018-04-04 17:13:09 +0545

    add new case to match fetch decks done condition

--- src/screens/Decks/reducers/index.js
+++ src/screens/Decks/reducers/index.js
@@ -7,7 +7,9 @@ const initialState = {
 const deck = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_DECK_DONE':
-      return {...state, items: [action.item, ...state.items]};
+      return { ...state, items: [action.item, ...state.items] };
+    case 'FETCH_DECKS_DONE':
+      return { ...state, items: action.items };
     default:
       return state
   }
