ec58daf2248b28d85eaba2a97cd3d06d19dfa144
sushanta <adhikariratna52@gmail.com>
2018-04-04 16:34:36 +0545

    add add deck functionality

--- src/screens/Decks/index.js
+++ src/screens/Decks/index.js
@@ -2,17 +2,54 @@
  * Created by sushanta on 4/4/18.
  */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addDeck } from './actions';
 
 class Decks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deckName: ''
+    };
+  }
+  componentDidMount() {
+
+  }
+  handleDeckNameInput = e => {
+    this.setState({
+      deckName: e.target.value
+    });
+  };
+  handleNewDeckSubmission = () => {
+    if (this.state.deckName === '') return;
+    this.props.addDeck(this.state.deckName);
+    this.setState({
+      deckName: ''
+    });
+  };
   render() {
     return (
       <div>
-        <span>
-          Decks of cards
-        </span>
+        <input type="text" value={this.state.deckName} onChange={this.handleDeckNameInput}/>
+        <button onClick={this.handleNewDeckSubmission}>Submit</button>
+        {this.props.decks.length > 0 && (
+          <div>
+            {this.props.decks.map(deck => (
+              <span>{deck.name}</span>
+            ))}
+          </div>
+        )}
       </div>
     );
   }
 }
 
-export default Decks;
+const mapStateToProps = state => {
+  return {
+    decks: state.deck.items
+  }
+};
+const mapDispatchToProps = {
+  addDeck
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Decks);
