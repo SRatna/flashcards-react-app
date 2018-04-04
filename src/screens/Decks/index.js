2b23cdfb812bd1791e39f7e526921f4ddd78b14d
sushanta <adhikariratna52@gmail.com>
2018-04-04 14:41:45 +0545

    add barebone decks component

---
+++ src/screens/Decks/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by sushanta on 4/4/18.
+ */
+import React, { Component } from 'react';
+
+class Decks extends Component {
+  render() {
+    return (
+      <div>
+        <span>
+          Decks of cards
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Decks;
