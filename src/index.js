b3f888a5a40374134803070c40c6bcfb61876c27
sushanta <adhikariratna52@gmail.com>
2018-04-04 13:25:45 +0545

    initial setup for redux n redux saga

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+import createSagaMiddleware from 'redux-saga'
+import saga from './saga';
 import App from './App'
 import './index.css'
 
-render(<App/>, document.querySelector('#app'))
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+);
+sagaMiddleware.run(saga);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#app')
+);
