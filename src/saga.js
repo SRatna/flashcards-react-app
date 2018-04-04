83a12be2e24c042110287c9d67ee19469678b02b
sushanta <adhikariratna52@gmail.com>
2018-04-04 17:02:10 +0545

    add fetchDecks watcher in root saga

--- src/saga.js
+++ src/saga.js
@@ -2,9 +2,10 @@
  * Created by sushanta on 4/4/18.
  */
 import { takeLatest, all } from 'redux-saga/effects';
-import { addDeck } from './screens/Decks/saga';
+import { addDeck, fetchDecks } from './screens/Decks/saga';
 export default function* watcher() {
   yield all([
-    takeLatest('ADD_DECK', addDeck)
+    takeLatest('ADD_DECK', addDeck),
+    takeLatest('FETCH_DECKS', fetchDecks)
   ]);
 }
