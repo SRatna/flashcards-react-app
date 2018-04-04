f300ec562921e4435fd9c9f70024ebd1decebbeb
sushanta <adhikariratna52@gmail.com>
2018-04-04 16:35:42 +0545

    add deck reducer in root reducer

--- src/reducer.js
+++ src/reducer.js
@@ -2,8 +2,9 @@
  * Created by sushanta on 4/4/18.
  */
 import { combineReducers } from 'redux';
-
+import deck from './screens/Decks/reducers';
 const appReducer = combineReducers({
+  deck
 });
 
 export default appReducer;
