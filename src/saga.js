e1edc04206f1a5fc5f3bf34d3db02eed0b398a6b
sushanta <adhikariratna52@gmail.com>
2018-04-04 16:34:04 +0545

    add addDeck watcher

--- src/saga.js
+++ src/saga.js
@@ -1,9 +1,10 @@
 /**
  * Created by sushanta on 4/4/18.
  */
-import { all } from 'redux-saga/effects';
-
+import { takeLatest, all } from 'redux-saga/effects';
+import { addDeck } from './screens/Decks/saga';
 export default function* watcher() {
   yield all([
+    takeLatest('ADD_DECK', addDeck)
   ]);
 }
