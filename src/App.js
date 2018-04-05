c68604d82272c2f99b283d79c570337e039bf9f3
sushanta <adhikariratna52@gmail.com>
2018-04-05 16:35:52 +0545

    add new route for cards page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import Decks from './screens/Decks';
+import Cards from './screens/Cards';
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Decks} />
+      <Route exact path="/cards/:deckID" component={Cards} />
     </Switch>
   </Router>
 );
