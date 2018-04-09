db7b3ec7b5a42999a0b0bdc9b9a7afe156f0ba0b
sushanta <adhikariratna52@gmail.com>
2018-04-09 09:53:09 +0545

    add setting to view bundles stats

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,14 @@
+var StatsPlugin = require('stats-webpack-plugin');
 module.exports = {
-  type: 'react-app'
-}
+  type: 'react-app',
+  webpack: {
+    extra: {
+      plugins: [
+        new StatsPlugin('stats.json', {
+          chunkModules: true,
+          exclude: [/node_modules[\\\/]react/]
+        })
+      ]
+    }
+  }
+};
