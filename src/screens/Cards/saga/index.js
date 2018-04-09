7820076cc22099ee8351ba470ae3113ed346a395
sushanta <adhikariratna52@gmail.com>
2018-04-09 09:52:09 +0545

    add sagas to fetch n add cards

---
+++ src/screens/Cards/saga/index.js
@@ -0,0 +1,31 @@
+/**
+ * Created by sushanta on 4/6/18.
+ */
+import { put } from 'redux-saga/effects';
+import db from '../../../db';
+import shortid from 'shortid';
+import { addCardDone, fetchCardsDone } from '../actions';
+
+export function* addCard({ item }) {
+  const cardItem = item;
+  try {
+    const id = yield db.cards.put({
+      id: shortid.generate(),
+      createdAt: Date.now(),
+      ...cardItem
+    });
+    const item = yield db.cards.get(id);
+    yield put(addCardDone(item));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function* fetchCards({ deckID }) {
+  try {
+    const items = yield db.cards.where('deckID').equals(deckID).toArray();
+    yield put(fetchCardsDone(items))
+  } catch (err) {
+    console.log(err);
+  }
+}
