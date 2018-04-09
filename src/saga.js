e3f099ac27cc4b9ae94452c93635d7b6b04bd122
sushanta <adhikariratna52@gmail.com>
2018-04-09 09:54:27 +0545

    add cards sagas to root saga file

--- src/saga.js
+++ src/saga.js
@@ -3,9 +3,12 @@
  */
 import { takeLatest, all } from 'redux-saga/effects';
 import { addDeck, fetchDecks } from './screens/Decks/saga';
+import { addCard, fetchCards } from './screens/Cards/saga';
 export default function* watcher() {
   yield all([
     takeLatest('ADD_DECK', addDeck),
-    takeLatest('FETCH_DECKS', fetchDecks)
+    takeLatest('FETCH_DECKS', fetchDecks),
+    takeLatest('ADD_CARD', addCard),
+    takeLatest('FETCH_CARDS', fetchCards)
   ]);
 }
