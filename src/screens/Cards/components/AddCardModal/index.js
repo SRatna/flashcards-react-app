1c80b63f762c9caddd41fbfe37ef88e503870c85
sushanta <adhikariratna52@gmail.com>
2018-04-10 12:01:12 +0545

    create a addCardModal component

---
+++ src/screens/Cards/components/AddCardModal/index.js
@@ -0,0 +1,77 @@
+/**
+ * Created by sushanta on 4/10/18.
+ */
+import React, { Component } from 'react';
+import './index.scss';
+
+class AddCardModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: '',
+      meaning: '',
+      examples: ''
+    }
+  }
+  handleSaveCardBtnClick = () => {
+    const { word, meaning, examples } = this.state;
+    if (word && meaning && examples) {
+      this.props.addCard({
+        word, meaning, examples, deckID: this.props.deckID
+      });
+      this.setState({
+        word: '',
+        meaning: '',
+        examples: ''
+      });
+      this.props.closeModal();
+    }
+  };
+  render() {
+    const { word, meaning, examples } = this.state;
+    const { visibility, closeModal } = this.props;
+    return (
+      <div
+        className={`add-card-modal ${visibility ? 'visible' : 'invisible'}`}>
+        <div className="content">
+          <div className="header">
+            <span className="title">
+              Add New Card
+            </span>
+            <span
+              onClick={closeModal}
+              className="close">
+              &times;
+            </span>
+          </div>
+          <div className="body">
+            <input
+              placeholder="Add word..."
+              type="text"
+              value={word}
+              onChange={e => {
+                this.setState({ word: e.target.value });
+              }} />
+            <textarea
+              placeholder="Add meaning..."
+              rows="3"
+              value={meaning}
+              onChange={e => {
+                this.setState({ meaning: e.target.value });
+              }} />
+            <textarea
+              placeholder="Add examples..."
+              rows="3"
+              value={examples}
+              onChange={e => {
+                this.setState({ examples: e.target.value });
+              }} />
+            <button onClick={this.handleSaveCardBtnClick}>Save</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AddCardModal;
