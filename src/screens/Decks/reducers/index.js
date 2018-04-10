90102b1effd77a70d153853303b6339c7a94a4ef
sushanta <adhikariratna52@gmail.com>
2018-04-10 16:27:04 +0545

    add new item at first

--- src/screens/Decks/reducers/index.js
+++ src/screens/Decks/reducers/index.js
@@ -7,7 +7,7 @@ const initialState = {
 const deck = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_DECK_DONE':
-      return { ...state, items: [...state.items, action.item] };
+      return { ...state, items: [action.item, ...state.items] };
     case 'FETCH_DECKS_DONE':
       return { ...state, items: action.items };
     default:
