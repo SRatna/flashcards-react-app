a4edd29a3df430ad539e82c88e544370c1fe39d3
sushanta <adhikariratna52@gmail.com>
2018-04-11 09:57:20 +0545

    add new route for practice page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import Decks from './screens/Decks';
 import Cards from './screens/Cards';
+import Practice from './screens/Practice';
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Decks} />
       <Route exact path="/cards/:deckID" component={Cards} />
+      <Route exact path="/practice/:deckID" component={Practice} />
     </Switch>
   </Router>
 );
