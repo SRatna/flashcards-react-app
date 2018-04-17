2f79fece8b4e5174a1ee6a29a0e2b7cb6bd03794
sushanta <adhikariratna52@gmail.com>
2018-04-17 12:18:55 +0545

    add delete deck saga to root saga

--- src/saga.js
+++ src/saga.js
@@ -2,11 +2,16 @@
  * Created by sushanta on 4/4/18.
  */
 import { takeLatest, all } from 'redux-saga/effects';
-import { addDeck, fetchDecks } from './screens/Decks/saga';
+import {
+  addDeck,
+  fetchDecks,
+  deleteDeck
+} from './screens/Decks/saga';
 import { addCard, fetchCards } from './screens/Cards/saga';
 export default function* watcher() {
   yield all([
     takeLatest('ADD_DECK', addDeck),
+    takeLatest('DELETE_DECK', deleteDeck),
     takeLatest('FETCH_DECKS', fetchDecks),
     takeLatest('ADD_CARD', addCard),
     takeLatest('FETCH_CARDS', fetchCards)
