b41c187b7dcded02b9f4d8ac39d5ab6ce8b81fab
sushanta <adhikariratna52@gmail.com>
2018-04-17 16:12:38 +0545

    add edit deck saga watcher to root saga

--- src/saga.js
+++ src/saga.js
@@ -5,7 +5,8 @@ import { takeLatest, all } from 'redux-saga/effects';
 import {
   addDeck,
   fetchDecks,
-  deleteDeck
+  deleteDeck,
+  editDeck
 } from './screens/Decks/saga';
 import { addCard, fetchCards } from './screens/Cards/saga';
 export default function* watcher() {
@@ -13,6 +14,7 @@ export default function* watcher() {
     takeLatest('ADD_DECK', addDeck),
     takeLatest('DELETE_DECK', deleteDeck),
     takeLatest('FETCH_DECKS', fetchDecks),
+    takeLatest('EDIT_DECK', editDeck),
     takeLatest('ADD_CARD', addCard),
     takeLatest('FETCH_CARDS', fetchCards)
   ]);
