4c1913790dd7c6aa703c8514905b21bcbbce8812
sushanta <adhikariratna52@gmail.com>
2018-04-19 12:12:22 +0545

    add edit functionality

--- src/screens/Cards/components/AddEditCardModal/index.js
+++ src/screens/Cards/components/AddEditCardModal/index.js
@@ -10,25 +10,37 @@ class AddEditCardModal extends Component {
     this.state = {
       word: '',
       meaning: '',
-      examples: ''
-    }
+      examples: '',
+      editMode: false,
+      cardId: null
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    const { cardData, editMode } = nextProps;
+    this.setState({
+      word: cardData.word || '',
+      meaning: cardData.meaning || '',
+      examples: cardData.examples || '',
+      editMode,
+      cardId: cardData.id || null
+    });
   }
   handleSaveCardBtnClick = () => {
-    const { word, meaning, examples } = this.state;
+    const { word, meaning, examples, editMode, cardId } = this.state;
+    const { addCard, editCard, closeModal, deckID} = this.props;
     if (word && meaning && examples) {
-      this.props.addCard({
-        word, meaning, examples, deckID: this.props.deckID
-      });
-      this.setState({
-        word: '',
-        meaning: '',
-        examples: ''
-      });
-      this.props.closeModal();
+      editMode
+        ? editCard({
+          word, meaning, examples, deckID, id: cardId
+        })
+        : addCard({
+          word, meaning, examples, deckID
+        });
+      closeModal();
     }
   };
   render() {
-    const { word, meaning, examples } = this.state;
+    const { word, meaning, examples, editMode } = this.state;
     const { visibility, closeModal } = this.props;
     return (
       <div
@@ -36,7 +48,7 @@ class AddEditCardModal extends Component {
         <div className="content">
           <div className="modal-header">
             <span className="modal-title">
-              Add New Card
+              {editMode ? 'Edit Card' : 'Add New Card'}
             </span>
           </div>
           <div className="body">
@@ -44,23 +56,17 @@ class AddEditCardModal extends Component {
               placeholder="Add word..."
               type="text"
               value={word}
-              onChange={e => {
-                this.setState({ word: e.target.value });
-              }} />
+              onChange={e => this.setState({ word: e.target.value })} />
             <textarea
               placeholder="Add meaning..."
               rows="5"
               value={meaning}
-              onChange={e => {
-                this.setState({ meaning: e.target.value });
-              }} />
+              onChange={e => this.setState({ meaning: e.target.value })} />
             <textarea
               placeholder="Add examples..."
               rows="5"
               value={examples}
-              onChange={e => {
-                this.setState({ examples: e.target.value });
-              }} />
+              onChange={e => this.setState({ examples: e.target.value })} />
             <div className="modal-actions">
               <button onClick={this.handleSaveCardBtnClick}>Save</button>
               <button onClick={closeModal}>Cancel</button>
